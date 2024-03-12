b639827f0d7e7ca97d4aac3cc31f84b2425b997a
Alvaro Kieling <atkwaves@gmail.com>
2024-03-12 20:35:57 -0300

    A02 Construindo o catálogo de filmes

--- primeiro-projeto/src/Main.java
+++ primeiro-projeto/src/Main.java
@@ -2,6 +2,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println(sinopse);
+
+        String sinopseTextBlocks = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopseTextBlocks);
+
+        int classificacao = (int) media / 2;
+        System.out.println(classificacao);
     }
 }
---
+++ primeiro-projeto/src/labs/a02/ExA02.java
@@ -0,0 +1,42 @@
+package labs.a02;
+
+public class ExA02 {
+
+    public static void main(String[] args) {
+
+        double nota01 = 10.0;
+        double nota02 = 8.0;
+        double media = (nota01 + nota02) / 2;
+
+        System.out.println("Média: " + media);
+
+        double variavelDouble = 7.5;
+        int variavelInt = (int) variavelDouble;
+
+        System.out.println("Casting de double para int: " + variavelInt);
+
+        char variavelChar = 'A';
+        String variavelString = "zul";
+
+        System.out.println("A palavra é: " + (variavelChar + variavelString));
+
+        double precoProduto = 15.00;
+        int quantidade = 2;
+        double valorTotal = precoProduto * quantidade;
+
+        System.out.println("O valor total da compra é R$" + valorTotal);
+
+        double valorEmDolares = 20;
+        double cotacaoDolar = 4.94;
+        double valorConvertidoReais = cotacaoDolar * valorEmDolares;
+
+        System.out.println(String.format("R$ %.2f", valorConvertidoReais));
+
+        double precoOriginal = 50.0;
+        double percentualDesconto = 10;
+        double valorDesconto = (precoOriginal * percentualDesconto) / 100;
+        double precoComDesconto = precoOriginal - valorDesconto;
+
+        System.out.println("O preço com desconto é R$" + precoComDesconto);
+    }
+}
